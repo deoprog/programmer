5163159d845ee94b6dfa1abdf35e340d0440d4d6
Robson Carneiro De Azevedo <robson.azevedo@cogerh.com.br>
2020-12-20 17:56:37 -0300

    muito arquivo mandando alguns

--- src/main/java/br/com/mobitbrasil/interview/services/ContatoService.java
+++ src/main/java/br/com/mobitbrasil/interview/services/ContatoService.java
@@ -4,6 +4,7 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
 
 import javax.websocket.server.PathParam;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -12,40 +13,48 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-@Path("/agenda")
+@Path("/api")
 @Produces({APPLICATION_XML, APPLICATION_JSON})
 public class ContatoService {
 
 	//recupera uma lsita com os 10 primeiros contatos ordenado pelo nome
 	@GET
-    @Path("/api/contatos")
+    @Path("/contatos")
     @Produces(APPLICATION_JSON)
     public Response list() {
 
-        return Response.ok().build();
+		return Response
+                .status(Response.Status.ACCEPTED)
+                .entity("Get").build();
     }
 	
 	//Cria um novo Contato a partir de um JSON enviado no body
 	@POST
-    @Path("/api/contatos")
+    @Path("/contatos")
     @Produces(APPLICATION_JSON)
+	@Consumes(APPLICATION_JSON)
     public Response add() {
 
-        return Response.ok().build();
+        return Response
+                .status(Response.Status.ACCEPTED)
+                .entity("Contatos").build();
     }
 	
 	//Atualiza um Contato a partir de um JSON enviado no body com o atributo Código existente;
 	@PUT
-    @Path("/api/contatos")
+    @Path("/contatos")
     @Produces(APPLICATION_JSON)
+	@Consumes(APPLICATION_JSON)
     public Response update() {
 
-        return Response.ok().build();
+        return Response
+                .status(Response.Status.ACCEPTED)
+                .entity("Alterado").build();
     }
 	
 	//Remove um contato a partir do código informado;
 	@DELETE
-    @Path("/api/contatos/{id}")
+    @Path("/contatos/{id}")
     @Produces(APPLICATION_JSON)
     public Response remove(@PathParam("id") Long id) {
 
@@ -54,18 +63,18 @@ public class ContatoService {
 	
 	//Retorna um JSON do contato referente ao código informado.
 	@GET
-    @Path("/api/contatos/{id}")
+    @Path("/contatos/{id}")
     @Produces(APPLICATION_JSON)
-    public Response getById(@PathParam("id") Long id) {
+    public Response findById(@PathParam("id") Long id) {
 
         return Response.ok().build();
     }
 	
 	//Retorna um JSON dos endereços do contato referente ao código informado.
 	@GET
-    @Path("/api/contatos/{id}/telefones")
+    @Path("/contatos/{id}/telefones")
     @Produces(APPLICATION_JSON)
-    public Response getEnderecoById(@PathParam("id") Long id) {
+    public Response findEnderecoById(@PathParam("id") Long id) {
 
         return Response.ok().build();
     }
